2ff2775bc5e6472cf26843f30a4f218f2bb5c25f
Adam Duff <j.adam.duff@gmail.com>
2018-11-07 22:33:14 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,10 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    Fraday.get 'https://api.github.com/search/repositories' do |req|
+      req.params['client_id'] = 'Iv1.272578ea28313f40'
+      req.params['client_secret'] = '3ecee81272dfb85c93ce417edb1d8b449e953987'
+      req.params['q'] = params['terms']
+    end
   end
 end
